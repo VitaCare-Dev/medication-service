8348d8d8562e8a8dfe1ee94668e499e11c1e9058
BriamCarrasco <briamcarrasco.f@gmail.com>
2026-06-26 02:21:17 -0400

    refactor: se crea clase de constantes

---
+++ src/main/java/com/grupo10/medication_service/constants/GlobalConstants.java
@@ -0,0 +1,16 @@
+package com.grupo10.medication_service.constants;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class GlobalConstants {
+    
+    public static final String HISTORY = "Historial de Toma";
+    public static final String MEDICATION = "Medicamento";
+    public static final String TIMESTAMP = "timestamp";
+    public static final String STATUS = "status";
+    public static final String ERROR = "error";
+    public static final String MESSAGE = "message";
+    public static final String ZONE_ID = "America/Santiago";
+
+}
